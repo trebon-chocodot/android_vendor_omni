665f640692db029d178cbbd47f0d92c60948ce42
android-armv7a-belalang-tempur <android.armv7a@gmail.com>
2014-03-11 20:32:13 +0700

    Update common.mk

--- config/common.mk
+++ config/common.mk
@@ -1,8 +1,8 @@
-PRODUCT_BRAND ?= omni
+PRODUCT_BRAND ?= Chocodot
 
 # bootanimation
 PRODUCT_COPY_FILES += \
-    vendor/omni/prebuilt/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+    vendor/Chocodot/prebuilt/bootanimation/bootanimation.zip:system/media/bootanimation.zip
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -29,33 +29,33 @@ endif
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
-    vendor/omni/prebuilt/bin/backuptool.sh:system/bin/backuptool.sh \
-    vendor/omni/prebuilt/bin/backuptool.functions:system/bin/backuptool.functions \
-    vendor/omni/prebuilt/bin/50-hosts.sh:system/addon.d/50-hosts.sh \
-    vendor/omni/prebuilt/bin/blacklist:system/addon.d/blacklist
+    vendor/Chocodot/prebuilt/bin/backuptool.sh:system/bin/backuptool.sh \
+    vendor/Chocodot/prebuilt/bin/backuptool.functions:system/bin/backuptool.functions \
+    vendor/Chocodot/prebuilt/bin/50-hosts.sh:system/addon.d/50-hosts.sh \
+    vendor/Chocodot/prebuilt/bin/blacklist:system/addon.d/blacklist
 
 # init.d support
 PRODUCT_COPY_FILES += \
-    vendor/omni/prebuilt/etc/init.d/00banner:system/etc/init.d/00banner \
-    vendor/omni/prebuilt/bin/sysinit:system/bin/sysinit
+    vendor/Chocodot/prebuilt/etc/init.d/00banner:system/etc/init.d/00banner \
+    vendor/Chocodot/prebuilt/bin/sysinit:system/bin/sysinit
 
 # userinit support
 PRODUCT_COPY_FILES += \
-    vendor/omni/prebuilt/etc/init.d/90userinit:system/etc/init.d/90userinit
+    vendor/Chocodot/prebuilt/etc/init.d/90userinit:system/etc/init.d/90userinit
 
 # Init script file with omni extras
 PRODUCT_COPY_FILES += \
-    vendor/omni/prebuilt/etc/init.local.rc:root/init.omni.rc
+    vendor/Chocodot/prebuilt/etc/init.local.rc:root/init.omni.rc
 
 # Enable SIP and VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
 # Additional packages
--include vendor/omni/config/packages.mk
+-include vendor/Chocodot/config/packages.mk
 
 # Versioning
--include vendor/omni/config/version.mk
+-include vendor/Chocodot/config/version.mk
 
 # Add our overlays
-PRODUCT_PACKAGE_OVERLAYS += vendor/omni/overlay/common
+PRODUCT_PACKAGE_OVERLAYS += vendor/Chocodot/overlay/common
